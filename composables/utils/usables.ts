8e9f678a39757a617cbc14a1f921fabe2d95bab3
Angelos Antoniades <anillusionist9050@gmail.com>
2025-06-03 15:22:28 +0300

    Simplified screen size check

--- composables/utils/usables.ts
+++ composables/utils/usables.ts
@@ -2,6 +2,8 @@ export const truncText = (id: string) => {
   return id?.length > 10 ? id.slice(0, 20) + "..." : id;
 };
 
-const $mobile = screen.width < 768 ? true : false;
+const $mobile = () => {
+  return screen.width < 780 ? true : false;
+};
 
 export { $mobile };
